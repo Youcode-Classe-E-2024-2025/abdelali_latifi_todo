e5a9917e6f1c60cbd2199d7bff11a71217dcb3f7
Latifi1937 <abdelalilatifi3@gmail.com>
2024-11-01 22:04:39 +0100

    modification en js et html

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const modal = document.getElementById('taskmodal');
             const todoCount = document.getElementById('todolist').children.length;
             const doingCount = document.getElementById('doinglist').children.length;
             const doneCount = document.getElementById('donelist').children.length;
-
             document.getElementById('todocounter').textContent = todoCount;
             document.getElementById('doingcounter').textContent = doingCount;
             document.getElementById('donecounter').textContent = doneCount;
@@ -16,21 +15,15 @@ const modal = document.getElementById('taskmodal');
         function addTaskToList(title, description, status, Priorité) {
             const taskItem = document.createElement('div');
             let borderColor;
-
-            switch (Priorité) {
-                case 'P1':
-                    borderColor = 'border-lime-500'; 
-                    break;
-                case 'P2':
-                    borderColor = 'border-orange-400'; 
-                    break;
-                case 'P3':
-                    borderColor = 'border-red-500'; 
-                    break;
-                default:
-                    borderColor = 'border-black'; 
-            }
-
+           if (Priorité === 'P1') {
+            borderColor = 'border-lime-500'; 
+           }
+           else if (Priorité === 'P2') {
+            borderColor = 'border-orange-400'; 
+        }
+           else if (Priorité === 'P3') {
+            borderColor = 'border-red-500'; 
+        }
             taskItem.className = `w-52 h-18 ${borderColor} border-l-8 rounded-r-lg bg-white my-5 justify-self-center flex justify-between items-center p-2`;
             taskItem.innerHTML = `
                 <div>
@@ -38,7 +31,6 @@ const modal = document.getElementById('taskmodal');
                     <textarea class="task-description" readonly>${description}</textarea>
                 </div>
             `;
-
             if (status === 'to-do') {
                 document.getElementById('todolist').appendChild(taskItem);
             } else if (status === 'doing') {
@@ -59,14 +51,11 @@ const modal = document.getElementById('taskmodal');
         
         taskForm.addEventListener('submit', (event) => {
             event.preventDefault();
-
             const title = document.getElementById('title').value;
             const description = document.getElementById('description').value;
             const status = document.getElementById('status').value;
             const priority = document.getElementById('Priorité').value; 
-
             addTaskToList(title, description, status, priority);
-
             modal.classList.add('hidden');
             taskForm.reset();
         });
